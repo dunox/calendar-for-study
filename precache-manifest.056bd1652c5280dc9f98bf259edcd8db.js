c565666667eced70dad4e7bdf527a07ffecb2e33
dunox <avshendrik@gmail.com>
2020-05-20 16:20:40 +0300

    Updates

---
+++ precache-manifest.056bd1652c5280dc9f98bf259edcd8db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b341cb2dd3a0a4c08ae42c40270729",
+    "url": "/calendar-for-study/index.html"
+  },
+  {
+    "revision": "b456873af27b57e84ea5",
+    "url": "/calendar-for-study/static/css/main.ef162fb2.chunk.css"
+  },
+  {
+    "revision": "0ff43ece5a835c6cc71d",
+    "url": "/calendar-for-study/static/js/2.ab929898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar-for-study/static/js/2.ab929898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b456873af27b57e84ea5",
+    "url": "/calendar-for-study/static/js/main.e91066eb.chunk.js"
+  },
+  {
+    "revision": "1d4327c6de676cc17597",
+    "url": "/calendar-for-study/static/js/runtime-main.043be56d.js"
+  },
+  {
+    "revision": "1e58e66ef838723a0c0f915e4cb4cbf8",
+    "url": "/calendar-for-study/static/media/clock.1e58e66e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calendar-for-study/static/media/logo.5d5d9eef.svg"
+  }
+]);
